e467f6a1e5ff2baf9dd8ae9a0c259dc838389284
huyenn24092001 <huyenn240901@gmail.com>
2022-01-18 13:03:25 +0700

    push1

---
+++ twitter.php
@@ -0,0 +1,111 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Twitter</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" 
+    integrity="sha512-Fo3rlrZj/k7ujTnHg4CGR2D7kSs0v4LLanw2qksYuRlEzO+tcaEPQogQ0KaoGN26/zrn20ImR1DfuLWnOo7aBA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Bangers&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="css/styles.css">
+
+</head>
+    
+<body >
+
+
+<main>
+<div class="main">
+  <div class="left">
+  <img src="images/login.jpeg" class="img-login" alt="...">
+  </div>
+<div class="right">
+  <div class="icon mt-5  ">
+  <i class="bi bi-twitter"></i>
+  </div>
+ <div class="middle">
+  
+   <h1> Đang diễn ra ngay bây giờ <h1>
+   <h4> Tham gia Twitter ngay hôm nay. <h4>
+   <a href="login.php" class="login"><i class="bi bi-apple"></i> Đăng ký bằng Apple</a>
+   <a href="login.php" class="login1">Đăng ký bằng số điện thoại hoặc email</a>
+   <h3 class= "h3 mt-5 "> Đã có tài khoản? <h3>
+   <a href="signUp.php" class="login2">Đăng nhập</a>
+ </div>
+</div>
+<footer class="main-footer" role =" contentinfo">
+   <ul>
+     <li>
+       <a href="#">Giới thiệu</a>
+     </li>
+     <li>
+       <a href="#">Trung tâm Trợ giúp</a>
+     </li>
+     <li>
+       <a href="#">Điều khoản Dịch vụ</a>
+     </li>
+     <li>
+       <a href="#">Chính sách Riêng tư</a>
+     </li>
+     <li>
+       <a href="#">Chính sách cookie</a>
+     </li>
+     <li>
+       <a href="#">Khả năng truy cập</a>
+     </li>
+     <li>
+       <a href="#">Thông tin quảng cáo</a>
+     </li>
+     <li>
+       <a href="#">Blog</a>
+     </li>
+     <li>
+       <a href="#">Trạng thái</a>
+     </li>
+     <li>
+       <a href="#">Nghề nghiệp</a>
+     </li>
+     <li>
+       <a href="#">Tài nguyên thương hiệu</a>
+     </li>
+     <li>
+       <a href="#">Quảng cáo</a>
+     </li>
+     <li>
+       <a href="#">Tiếp thị</a>
+     </li>
+     <li>
+       <a href="#">Twitter cho doanh nghiệp</a>
+     </li>
+     <li>
+       <a href="#">Nhà phát triển</a>
+     </li>
+     <li>
+       <a href="#">Danh mục</a>
+     </li>
+     <li>
+       <a href="#">Cài đặt</a>
+     </li>
+     <li>
+       <a href="#">  &copy;<?php echo date('Y');?>Twitter,Inc.</a>
+     </li>
+
+   </ul>
+
+</footer>
+</div>
+
+</main>
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" 
+integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+</body>
+</html>
